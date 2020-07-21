6f850d22ce64dd3bd660e153bf6d474454fe6656
sannyuni33 <sannyuni33@naver.com>
2020-07-21 16:48:19 +0900

    2020.07.21 commit from yoon
    1. 카메라 사용 안하는 모듈 import 문 삭제
    2. 카메라 출력 메시지 변경 및 삭제
    3. 불필요한 주석문 삭제
    4. 불필요한 else 문 삭제
    5. captureImage 메소드 불필요한 print(), time.sleep() 삭제

--- CameraRPI/cameraClient_test_0213.py
+++ CameraRPI/cameraClient_test_0213.py
@@ -1,16 +1,16 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import sys
 import os
 import struct
 import time
 from socket import *
 from threading import Thread
-import datetime
 from picamera import PiCamera
 chTime = 5
 tcpClientA = None
-BUFF_SIZE=1024
-camera=PiCamera()
+BUFF_SIZE = 1024
+camera = PiCamera()
+
 
 class ClientThread(Thread):
     def __init__(self):
@@ -24,41 +24,37 @@ class ClientThread(Thread):
         global chTime
         tcpClientA = socket(AF_INET, SOCK_STREAM)
         tcpClientA.connect((host, port))
-        print("서버랑 연결되었습니다")
+        print("서버와 연결되었습니다")
 
         while True:
             data = tcpClientA.recv(BUFFER_SIZE)
             msg = data.decode('utf-8')
             if msg == 'start':
-                print("광고 시작 메시지를 받았습니당")
+                print("광고시작 메시지를 받았습니당")
                 newThread = CameraWork(tcpClientA)
                 newThread.setDaemon(True)
                 newThread.start()
             elif msg == 'pause':
-
                 newThread.setStop()
-                print("일시 정지 메시지를 받았습니다")
-                # 쓰레드 자체를 실제로 종료시킴! 강제 종료도 아님!
-                # CameraWork 객체의 onOff 가 False 니까 반복문이 멈추고 run() 이 끝나기 때문
+                print("광고중단 메시지를 받았습니당")
             elif msg == 'exit':
                 print("빠이욤")
                 break
             elif int(msg) <= 100:
                 chTime = int(msg)
-                print("카메라 주기"+str(chTime)+"초로 변경합니다.")
-            else:
-                print("오류")
-                break
+                print("카메라 촬영 주기 변경: "+str(chTime)+"초")
         tcpClientA.close()
+
 
 class CameraWork(Thread):
     def __init__(self, tcpClientA):
         Thread.__init__(self)
         self.clientSocket = tcpClientA
-        self.FILE_NAME = ('image.jpg')
+        self.FILE_NAME = 'image.jpg'
         self.onOff = True
         global camera
         global chTime
+
     def setStop(self):
         self.onOff = False
 
@@ -72,10 +68,7 @@ class CameraWork(Thread):
 
     def captureImage(self, FILE_NAME, chTime):
         print("찍습니다")
-        time.sleep(chTime/2)
-        print(chTime)
-        time.sleep(chTime/2)
-        print(chTime/2)
+        time.sleep(chTime)
         camera.capture('/home/pi/sang/'+FILE_NAME)
 
     def sendImage(self, FILE_NAME):
@@ -93,15 +86,12 @@ class CameraWork(Thread):
                 self.clientSocket.send(i)
                 i = f.read(BUFF_SIZE)
             f.close()
-
             print('(check) ' + FILE_NAME + ' transfer complete')
-
         except Exception as e:
             sys.exit()
 
 
 if __name__ == '__main__':
-
     clientThread = ClientThread()
     clientThread.start()
 
